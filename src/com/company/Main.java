fccda214251a9cd6d99b5dc7bb610b06f8878ac7
Oona Carbonie <carbonie.oona@gmail.com>
2019-04-18 16:08:28 -0400

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i = 1; i < 10; i++){
-            for(int j = 1; j < i; j++) {
+            for(int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
             }
             System.out.println();
         }
     }
 }
+
